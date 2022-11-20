8659bceea031da5ac3f179ce32197edd77d49679
Nikita Pushchin <vbnekit@p5y.su>
2022-11-20 12:49:23 +0300

    spacing fix in last modified files
    1. fixed cflf/lfcr - git clients will automatically get correct line endings
    2. removed unnecessary trailing spaces
    3. each `tab` has been expanded to 4x-`space` - for proper visibility of more IDEs.
    4. some changes in spaces for more uniformity of the code

--- metamod/linkplug.cpp
+++ metamod/linkplug.cpp
@@ -34,9 +34,9 @@
  *
  */
 
-#include <extdll.h>		// always
+#include <extdll.h>     // always
 
-#include "linkent.h"	// LINK_ENTITY_TO_PLUGIN
+#include "linkent.h"    // LINK_ENTITY_TO_PLUGIN
 
 // Entity lists for plugins
 LINK_ENTITY_TO_PLUGIN(adminmod_timer, "adminmod")
