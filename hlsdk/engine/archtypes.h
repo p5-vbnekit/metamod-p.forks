33bcf4c3c2bdb0f516ed53f68383ffcad251909c
Nikita Pushchin <vbnekit@p5y.su>
2022-11-21 16:11:30 +0300

    hlsdk/engine/archtypes.h - refactored

--- hlsdk/engine/archtypes.h
+++ hlsdk/engine/archtypes.h
@@ -2,40 +2,27 @@
 // Word size dependent definitions
 // DAL 1/03
 //
+
 #ifndef ARCHTYPES_H
 #define ARCHTYPES_H
 
-#ifdef __x86_64__
-#define X64BITS
-#endif
+#include <cstddef>
 
-#if defined( _WIN32 ) && (! defined( __MINGW32__ ))
-
-typedef __int16 int16;
-typedef unsigned __int16 uint16;
-typedef __int32 int32;
-typedef unsigned __int32 uint32;
-typedef __int64 int64;
-typedef unsigned __int64 uint64;
-typedef __int32 intp;				// intp is an integer that can accomodate a pointer
-typedef unsigned __int32 uintp;		// (ie, sizeof(intp) >= sizeof(int) && sizeof(intp) >= sizeof(void *)
-
-#else /* _WIN32 */
-
-typedef short int16;
-typedef unsigned short uint16;
-typedef int int32;
-typedef unsigned int uint32;
-typedef long long int64;
-typedef unsigned long long uint64;
-#ifdef X64BITS
-typedef long long intp;
-typedef unsigned long long uintp;
+#if defined(__cplusplus) && (201103L <= __cplusplus)
+#include <cstdint>
 #else
-typedef int intp;
-typedef unsigned int uintp;
+#include "stdint.h"
 #endif
 
-#endif /* else _WIN32 */
+
+typedef ::int16_t int16;
+typedef ::uint16_t uint16;
+typedef ::int32_t int32;
+typedef ::uint32_t uint32;
+typedef ::int64_t int64;
+typedef ::uint64_t uint64;
+
+typedef ::std::ptrdiff_t    intp;   // intp is an integer that can accomodate a pointer
+typedef ::size_t            uintp;  // (ie, sizeof(intp) >= sizeof(int) && sizeof(intp) >= sizeof(void *)
 
 #endif /* ARCHTYPES_H */
