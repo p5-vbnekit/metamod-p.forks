456a2f9d3148c9d8a81949c091eadd296dcd4864
Nikita Pushchin <vbnekit@p5y.su>
2023-01-04 04:32:39 +0300

    h_export files - refactoring

--- metamod/h_export.h
+++ metamod/h_export.h
@@ -37,13 +37,11 @@
 #ifndef H_EXPORT_H
 #define H_EXPORT_H
 
-#include "osdep.h"			// DLLEXPORT, WINAPI, etc
+#include "osdep.h" // DLLEXPORT, WINAPI, etc
 
 // Our GiveFnptrsToDll, called by engine.
-typedef void (WINAPI *GIVE_ENGINE_FUNCTIONS_FN) (enginefuncs_t 
-		*pengfuncsFromEngine, globalvars_t *pGlobals);
+typedef void (WINAPI *GIVE_ENGINE_FUNCTIONS_FN) (enginefuncs_t *, globalvars_t *);
 
-C_DLLEXPORT void WINAPI GiveFnptrsToDll(enginefuncs_t *pengfuncsFromEngine, 
-		globalvars_t *pGlobals);
+C_DLLEXPORT void WINAPI GiveFnptrsToDll(enginefuncs_t *engine_functions, globalvars_t *global_variables);
 
 #endif /* H_EXPORT_H */
--- stub_plugin/h_export.cpp
+++ stub_plugin/h_export.cpp
@@ -5,11 +5,11 @@
 
 /***
 *
-*	Copyright (c) 1999, 2000 Valve LLC. All rights reserved.
-*	
-*	This product contains software technology licensed from Id 
-*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc. 
-*	All Rights Reserved.
+*   Copyright (c) 1999, 2000 Valve LLC. All rights reserved.
+*
+*   This product contains software technology licensed from Id
+*   Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc.
+*   All Rights Reserved.
 *
 *   Use, distribution, and modification of this source code and/or resulting
 *   object code is restricted to non-commercial enhancements to products from
@@ -27,6 +27,8 @@
 
 #include <extdll.h>
 
+#include <cstring>
+
 #include <h_export.h>
 
 // From SDK dlls/h_export.cpp:
@@ -38,8 +40,7 @@ globalvars_t  *gpGlobals;
 // Receive engine function table from engine.
 // This appears to be the _first_ DLL routine called by the engine, so we
 // do some setup operations here.
-void WINAPI GiveFnptrsToDll( enginefuncs_t* pengfuncsFromEngine, globalvars_t *pGlobals )
-{
-	memcpy(&g_engfuncs, pengfuncsFromEngine, sizeof(enginefuncs_t));
-	gpGlobals = pGlobals;
+void WINAPI GiveFnptrsToDll(enginefuncs_t *engine_functions, globalvars_t *global_variables) {
+    ::std::memcpy(&g_engfuncs, engine_functions, sizeof(enginefuncs_t));
+    gpGlobals = global_variables;
 }
