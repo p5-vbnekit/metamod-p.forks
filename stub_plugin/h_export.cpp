fd23aaf57ddeaba5a2833d00447f0df3475e63f4
Nikita Pushchin <vbnekit@p5y.su>
2023-01-09 16:57:48 +0300

    decoration [spacing] changes before patching h_export files
    The basic things that can make it more difficult to code review in various utilities should be eliminated.
    
    1. added last `eof` character if needed
    2. removed unnecessary/added needed spaces
    3. fixed cflf/lfcr - git clients will automatically get correct line endings
    4. each `tab` has been expanded to 4x-`space` - for proper visibility of more IDEs.

--- stub_plugin/h_export.cpp
+++ stub_plugin/h_export.cpp
@@ -5,11 +5,11 @@
 
 /***
 *
-*	Copyright (c) 1999, 2000 Valve LLC. All rights reserved.
-*	
-*	This product contains software technology licensed from Id 
-*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc. 
-*	All Rights Reserved.
+*   Copyright (c) 1999, 2000 Valve LLC. All rights reserved.
+*
+*   This product contains software technology licensed from Id
+*   Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc.
+*   All Rights Reserved.
 *
 *   Use, distribution, and modification of this source code and/or resulting
 *   object code is restricted to non-commercial enhancements to products from
@@ -29,17 +29,17 @@
 
 #include <h_export.h>
 
+
 // From SDK dlls/h_export.cpp:
 
 //! Holds engine functionality callbacks
 enginefuncs_t g_engfuncs;
-globalvars_t  *gpGlobals;
+globalvars_t *gpGlobals;
 
 // Receive engine function table from engine.
 // This appears to be the _first_ DLL routine called by the engine, so we
 // do some setup operations here.
-void WINAPI GiveFnptrsToDll( enginefuncs_t* pengfuncsFromEngine, globalvars_t *pGlobals )
-{
-	memcpy(&g_engfuncs, pengfuncsFromEngine, sizeof(enginefuncs_t));
-	gpGlobals = pGlobals;
+void WINAPI GiveFnptrsToDll(enginefuncs_t* pengfuncsFromEngine, globalvars_t *pGlobals) {
+    memcpy(&g_engfuncs, pengfuncsFromEngine, sizeof(enginefuncs_t));
+    gpGlobals = pGlobals;
 }
--- wdmisc_plugin/h_export.cpp
+++ wdmisc_plugin/h_export.cpp
@@ -7,11 +7,11 @@
 
 /***
 *
-*	Copyright (c) 1999, 2000 Valve LLC. All rights reserved.
-*	
-*	This product contains software technology licensed from Id 
-*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc. 
-*	All Rights Reserved.
+*   Copyright (c) 1999, 2000 Valve LLC. All rights reserved.
+*
+*   This product contains software technology licensed from Id
+*   Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc.
+*   All Rights Reserved.
 *
 *   Use, distribution, and modification of this source code and/or resulting
 *   object code is restricted to non-commercial enhancements to products from
@@ -32,18 +32,18 @@
 #include <h_export.h>
 #include <meta_api.h>
 
+
 //! Holds engine functionality callbacks
 enginefuncs_t g_engfuncs;
-globalvars_t  *gpGlobals;
+globalvars_t *gpGlobals;
 
 // Receive engine function table from engine.
 // This appears to be the _first_ DLL routine called by the engine, so we
 // do some setup operations here.
-void WINAPI GiveFnptrsToDll( enginefuncs_t* pengfuncsFromEngine, globalvars_t *pGlobals )
-{
-	memcpy(&g_engfuncs, pengfuncsFromEngine, sizeof(enginefuncs_t));
-	gpGlobals = pGlobals;
-	// NOTE!  Have to call logging function _after_ copying into g_engfuncs, so
-	// that g_engfuncs.pfnAlertMessage() can be resolved properly, heh. :)
-	UTIL_LogPrintf("[%s] dev: called: GiveFnptrsToDll\n", Plugin_info.logtag);
+void WINAPI GiveFnptrsToDll(enginefuncs_t* pengfuncsFromEngine, globalvars_t *pGlobals) {
+    memcpy(&g_engfuncs, pengfuncsFromEngine, sizeof(enginefuncs_t));
+    gpGlobals = pGlobals;
+    // NOTE!  Have to call logging function _after_ copying into g_engfuncs, so
+    // that g_engfuncs.pfnAlertMessage() can be resolved properly, heh. :)
+    UTIL_LogPrintf("[%s] dev: called: GiveFnptrsToDll\n", Plugin_info.logtag);
 }
